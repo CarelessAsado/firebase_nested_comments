3cc80f26c0bf26383337c44c7cb70e79d6e3463d
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-09-28 17:05:03 -0300

    cors fixed

--- back/src/constants/index.ts
+++ back/src/constants/index.ts
@@ -1,6 +1,6 @@
 import { CookieOptions } from "express";
 export const FRONTEND_URL = process.env.NODE_ENV
-  ? "https://62e1b83ac9af450008fa268c--amazing-wright-70ac6b.netlify.app"
+  ? "https://amazing-wright-70ac6b.netlify.app"
   : "http://localhost:3000";
 
 export const JWT_SECRET = process.env.JWT_SECRET as string;
