6014a4fa2feadc4bf2e2f1b83cb1c69b73dbf642
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-10-27 21:27:59 -0300

    perfecting mongo aggregate pipeline/ subquery for each parent component

--- back/src/controllers/comment.ts
+++ back/src/controllers/comment.ts
@@ -43,28 +43,107 @@ export const getAllTasks = errorWrapper(async (req, res, next) => {
     { path: "userID", select: "img username _id" }
   ); */
   type IFacet = { commentsData: IDirectory[] };
+
   const facet = await Comment.aggregate<IFacet>([
-    {
-      //hacer un userID dinamico
-      //ver si se puede hacer $lookup adentro de $facet
-      //$lookup requires either 'pipeline' or both 'localField' and 'foreignField' to be specified
-      $lookup: {
-        from: "users", //usé mongosh p/ver los nombres de las collections
-        as: "userID", //le podés poner el nombre q quieras
-        localField: "userID",
-        foreignField: "_id",
-      },
-    },
+    { $match: { path: "" } },
+
+    //ver si se puede hacer $lookup adentro de $facet
+    //$lookup requires either 'pipeline' or both 'localField' and 'foreignField' to be specified
+
     //x alguna razón $lookup devuelve un array, entonces lo vuelvo a transformar en object con $unwind
     //otras posibles alternativas: https://stackoverflow.com/questions/41602831/convert-a-lookup-result-to-an-object-instead-of-array
-    { $unwind: "$userID" },
+    /* { $unwind: "$userID" }, */
     //hacer un project p/ seleccionar solo _id,username,email,img
     /*    { $project: { "userID.username": 1, "userID.email": 1, "userID.img": 1 } }, */
     //esto no sirve xq elimino todos los otros campos, creo q tengo q usar la opcion pipeline dentro de lookup, ver
+    //Por c/object en al cual le hago populate, tmb puedo hacer una query adicional, y agregarle extra info
     //https://www.mongodb.com/docs/manual/reference/operator/aggregation/lookup/#perform-an-uncorrelated-subquery-with--lookup
     {
       $facet: {
-        commentsData: [{ $skip: (page - 1) * limit }, { $limit: limit }],
+        commentsData: [
+          /*      {
+            //hacer un userID dinamico
+            //ver si se puede hacer $lookup adentro de $facet
+            //$lookup requires either 'pipeline' or both 'localField' and 'foreignField' to be specified
+            $lookup: {
+              from: "users", //usé mongosh p/ver los nombres de las collections
+              as: "userID", //le podés poner el nombre q quieras
+              localField: "userID",
+              foreignField: "_id",
+            },
+          }, */
+          //por c/object vuelve
+          /*    {
+            $addFields: {
+              nested: {
+                $match: {
+                  path: { $in: ["6355d1207f88f3934f46bf62"] },
+                },
+                    $regexMatch: {
+                  input: "$path",
+                  regex: "6355d1207f88f3934f46bf62",
+                }, 
+              },
+            },
+          }, */
+          //creo q no anda, xq el primer $lookup viene adentro de un array, mmm no sé, creo q el array estaria en userID, no tiene nada q ver con _id
+
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+
+          //una vez q limité el nro de PARENT CONV, hago el populate del user id y la subquery
+
+          {
+            $lookup: {
+              from: "users", //usé mongosh p/ver los nombres de las collections
+              as: "userID", //le podés poner el nombre q quieras
+              localField: "userID",
+              foreignField: "_id",
+            },
+          },
+
+          { $unwind: "$userID" },
+
+          //SUBQUERY PER PARENT CONVERSATION
+          {
+            $lookup: {
+              from: "comments", //usé mongosh p/ver los nombres de las collections
+              as: "nested", //le podés poner el nombre q quieras
+              //TUVE Q STRINGIFY EL _id xq sino $regexMatch se quejaba q el regex value no era el indicado
+              let: { parentID: { $toString: "$_id" } },
+              pipeline: [
+                {
+                  //cuando usas match con let variables, tenés q recurrir a $expr (esta en la doc, anotar bien dsp)
+                  $match: {
+                    $expr: {
+                      $regexMatch: {
+                        input: "$path",
+                        regex: "$$parentID", //Your text search here
+                      },
+
+                      /* $match: {
+                    path: { $convert: { input: "$_id", to: "string" } },
+                  }, */
+                      /*  $match: {
+                    path: { $in: ["6355d1207f88f3934f46bf62"] },
+                  }, */
+                      /*             esto anda, lo usé a modo de prueba. Primer check q logró subquery dentro de la misma collection    $ne: ["$path", "$$parentID"], */
+                    },
+                  },
+                },
+                { $count: "totalSubquery" },
+              ],
+            },
+          },
+          //el problema en realidad es q count si no hay docs, devuelve nada, y cuando hago unwind es como q desaparece "nested" key. Se me ocurre cotejar si el array esta vacio o no, y en base a eso agregar count:0. O podria hacer dsp, si nested no existe, agregar nested:0
+          /* {
+            $unwind: {
+              path: "$nested",
+              //a veces no hay children, si no pongo esto, al hacer unwind al empty array es como q no lo envia dsp al front
+              preserveNullAndEmptyArrays: true,
+            },
+          }, */
+        ],
         count: [{ $count: "totalComments" }],
       },
     },
